b81abdfe6ad4bba18d205484f549d85cd9095411
Viktor Prokopyev <prokopyevviktor@yandex.ru>
2021-08-02 14:27:49 +0300

    rename Data to Directory

--- src/api/functions.ts
+++ src/api/functions.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
-import { Data } from '../reducer/state';
+import { Directory } from '../reducer/state';
 
-export const getRootChildren = async (): Promise<Data[]> => {
+export const getRootChildren = async (): Promise<Directory[]> => {
     const { data } = await axios('http://164.90.161.80:3000/api/content')
     return data.children
 }
 
-export const getNewChildren = async (id: number): Promise<Data> => {
+export const getNewChildren = async (id: number): Promise<Directory[]> => {
     const { data } = await axios(`http://164.90.161.80:3000/api/content?dirId=${id}`)
-    return data
+    return data.children
 }
